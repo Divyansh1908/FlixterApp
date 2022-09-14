d8b01efc947aadf10c64332eddb619774662a0af
Divyansh Singh <divyansh301@gmail.com>
2022-09-14 15:39:15 -0700

    Created a repository for the completer Flix App

---
+++ Flix/Flix/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  Flix
+//
+//  Created by Divyansh Singh on 13/09/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
